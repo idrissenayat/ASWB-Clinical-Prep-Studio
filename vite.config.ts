d04db97843ba7c6f914a91ad5ec56cf822b42381
Idriss Enayat <idriss.enayat@gmail.com>
2026-05-13 11:31:05 -0400

    Fix local sharing base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/ASWB-Clinical-Prep-Studio/",
+  base: process.env.GITHUB_PAGES === "true" ? "/ASWB-Clinical-Prep-Studio/" : "/",
   plugins: [react()],
 });
